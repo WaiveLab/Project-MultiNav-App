d33b15c9cb430831e3c995f259bac0e1d8d5bf2b
mylestt <mylesthornelltimmer@gmail.com>
2026-06-08 14:21:15 -0400

    Initial Commit

---
+++ Project MultiNav App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project MultiNav App
+//
+//  Created by WAIVE lab on 6/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
